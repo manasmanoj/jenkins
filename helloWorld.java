d9c4408c1b8cbdb32b335e221c0101723c834b18
manasmanoj <mmkvm333@gmail.com>
2025-04-02 11:28:48 +0530

    jenkins first

---
+++ helloWorld.java
@@ -0,0 +1,6 @@
+public class helloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello Guys! First Jenkins Build is SUCCESSFULL!");
+		System.out.println("Check for new Build is SUCCESSFULL!");
+	}
+}
